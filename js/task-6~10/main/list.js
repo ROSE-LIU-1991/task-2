1c54e585f7024bda56de7f39dfc3a2305ad39a9f
wusanshuei <744739360@qq.com>
2018-10-09 00:57:44 +0800

    article

--- js/task-6~10/main/list.js
+++ js/task-6~10/main/list.js
@@ -1,31 +1,57 @@
-myApp.controller('ArticleListCtrl',function($scope){
-    $scope.abcd =[
-    {
-        a:'序号'
-    },
-    {
-        a:'图片'
-    },
-    {
-        a:'标题'
-    },
-    {
-        a:'类型'
-    },
-    {
-        a:'创建者'
-    },
-    {
-        a:'创建时间'
-    },
-    {
-        a:'修改时间'
-    },
-    {
-        a:'状态'
-    },
-    {
-        a:'操作'
-    }
+myApp.controller('ArticleListCtrl', function ($scope, $http) {
+    $scope.list = [{
+            a: '序号'
+        },
+        {
+            a: '图片'
+        },
+        {
+            a: '标题'
+        },
+        {
+            a: '类型'
+        },
+        {
+            a: '创建者'
+        },
+        {
+            a: '创建时间'
+        },
+        {
+            a: '修改时间'
+        },
+        {
+            a: '状态'
+        },
+        {
+            a: '操作'
+        }
     ];
+
+    function article() {
+        $http({
+            method: "get",
+            url: "/carrots-admin-ajax/a/article/search",
+        }).then(function (res) {
+                console.log(res);
+                console.log("获取成功");
+                $scope.articleList = res.data.data.articleList;
+                console.log($scope.articleList);
+            },
+            function (res) {
+                console.log("获取失败")
+            });
+    }
+    article();
 });
+myApp.constant('onLine',{
+    1:'上线',
+    2:'草稿'
+});
+
+myApp.filter('putaway',function(onLine){
+    return function(index){
+        return onLine[index]
+    }
+})
+//状态过滤
